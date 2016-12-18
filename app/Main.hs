60c210983f6718926a8d2d3ab9c446857ebb2021
Amar Shah <amar47shah@gmail.com>
2016-12-18 18:30:10 -0500

    Connect, read events, and just print them

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,19 @@
 module Main where
 
-import Lib
+import Control.Monad (replicateM_)
+import Network (PortID (PortNumber), accept, Socket, listenOn)
+import System.IO (hClose, hGetLine)
 
 main :: IO ()
-main = someFunc
+main = do
+  sockSource <- listen 9090
+  (source, _, _) <- accept sockSource
+  sockClient <- listen 9099
+  replicateM_ 100 $ do
+    (client, _, _) <- accept sockClient
+    hClose client
+  replicateM_ 10000000 $ hGetLine source >>= putStrLn
+  hClose source
+
+listen :: Int -> IO Socket
+listen = listenOn . PortNumber . fromIntegral
