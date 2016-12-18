fa19607d021ebf7764620656d94b434268217308
Amar Shah <amar47shah@gmail.com>
2016-12-18 18:32:20 -0500

    Parse events

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,7 @@
 module Main where
 
 import Control.Monad (replicateM_)
+import Data.List (unfoldr)
 import Network (PortID (PortNumber), accept, Socket, listenOn)
 import System.IO (hClose, hGetLine)
 
@@ -12,8 +13,35 @@ main = do
   replicateM_ 100 $ do
     (client, _, _) <- accept sockClient
     hClose client
-  replicateM_ 10000000 $ hGetLine source >>= putStrLn
+  replicateM_ 10000000 $ hGetLine source >>= print . parseEvent
   hClose source
 
 listen :: Int -> IO Socket
 listen = listenOn . PortNumber . fromIntegral
+
+data Event = Message  User User
+           | Follow   User User
+           | Unfollow User User
+           | Update   User
+           | Broadcast
+           deriving Show
+
+type User = String
+
+parseEvent :: String -> (Integer, Event)
+parseEvent = match . splitOn '|'
+      where
+  match :: [String] -> (Integer, Event)
+  match (n:"P":from:to:[]) = (read n, Message  from to)
+  match (n:"F":from:to:[]) = (read n, Follow   from to)
+  match (n:"U":from:to:[]) = (read n, Unfollow from to)
+  match (n:"S":from   :[]) = (read n, Update   from)
+  match (n:"B"        :[]) = (read n, Broadcast)
+  match _                  = error "Unrecognized event"
+
+splitOn :: Eq a => a -> [a] -> [[a]]
+splitOn = unfoldr . splitOnceOn
+      where
+  splitOnceOn :: Eq a => a -> [a] -> Maybe ([a], [a])
+  splitOnceOn _ [] = Nothing
+  splitOnceOn d xs = Just . (drop 1 <$>) $ break (== d) xs
