701c2d894bdfc26ef0e1aef73e1cfc1fb6308d9f
Amar Shah <amar47shah@gmail.com>
2016-12-22 21:26:47 -0500

    Use Integer UserIds

--- src/Event.hs
+++ src/Event.hs
@@ -3,7 +3,7 @@ module Event (Event (Event), Notification, UserId, Comm(..), parseEvent) where
 import Data.List (unfoldr)
 
 type Notification = String
-type UserId       = String
+type UserId       = Integer
 
 data Event = Event
   { eventRaw  :: Notification
@@ -21,10 +21,10 @@ parseEvent :: String -> Event
 parseEvent = match <*> splitOn '|'
       where
   match :: String -> [String] -> Event
-  match r (n:"P":from:to:[]) = Event r (read n) $ Message  from to
-  match r (n:"F":from:to:[]) = Event r (read n) $ Follow   from to
-  match r (n:"U":from:to:[]) = Event r (read n) $ Unfollow from to
-  match r (n:"S":from   :[]) = Event r (read n) $ Update   from
+  match r (n:"P":from:to:[]) = Event r (read n) $ Message  (read from) (read to)
+  match r (n:"F":from:to:[]) = Event r (read n) $ Follow   (read from) (read to)
+  match r (n:"U":from:to:[]) = Event r (read n) $ Unfollow (read from) (read to)
+  match r (n:"S":from   :[]) = Event r (read n) $ Update   (read from)
   match r (n:"B"        :[]) = Event r (read n) $ Broadcast
   match _ _                  = error "Unrecognized event"
 
--- src/Server.hs
+++ src/Server.hs
@@ -14,21 +14,19 @@ import Data.Set (Set)
 import System.IO (Handle, hGetLine)
 
 data Server = Server
-  { connections   :: TVar (Map UserId Client)
-  , followers     :: TVar (Map UserId (Set UserId))
+  { connections :: TVar (Map UserId Client)
+  , followers   :: TVar (Map UserId (Set UserId))
   }
 
 initServer :: IO Server
-initServer = Server
-         <$> newTVarIO Map.empty
-         <*> newTVarIO Map.empty
+initServer = Server <$> newTVarIO Map.empty <*> newTVarIO Map.empty
 
 serveUser :: Server -> Handle -> IO ()
 serveUser s h = getClient s h >>= beNotified
 
 getClient :: Server -> Handle -> IO Client
 getClient s h = do
-  u <- hGetLine h
+  u <- read <$> hGetLine h
   atomically $ do
     client <- newClient u h
     modifyTVar' (connections s) $ Map.insert u client
@@ -52,18 +50,18 @@ getFollowers s f = Map.findWithDefault Set.empty f <$> readTVar (followers s)
 allUsers :: Server -> STM (Set UserId)
 allUsers s = Map.keysSet <$> readTVar (connections s)
 
-follow :: Server -> UserId -> UserId -> STM ()
-follow s f = modifyTVar' (followers s) . Map.alter (addFollower f)
-
-unfollow :: Server -> UserId -> UserId -> STM ()
-unfollow s f = modifyTVar' (followers s) . Map.alter (removeFollower f)
-
 notify :: Server -> Notification -> UserId -> STM ()
 notify s r t = readTVar (connections s)
            >>= maybe (pure ()) (sendMessage r) . Map.lookup t
 
 notifyAll :: Server -> Notification -> Set UserId -> STM ()
 notifyAll s r ts = forM_ ts $ notify s r
+
+follow :: Server -> UserId -> UserId -> STM ()
+follow s f = modifyTVar' (followers s) . Map.alter (addFollower f)
+
+unfollow :: Server -> UserId -> UserId -> STM ()
+unfollow s f = modifyTVar' (followers s) . Map.alter (removeFollower f)
 
 addFollower :: Ord a => a -> Maybe (Set a) -> Maybe (Set a)
 addFollower f Nothing   = Just $ Set.insert f Set.empty
