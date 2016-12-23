05b70116009a686b89f39d071fe87c6b110be2c1
Amar Shah <amar47shah@gmail.com>
2016-12-22 22:37:28 -0500

    Use hlint

--- src/Event.hs
+++ src/Event.hs
@@ -21,11 +21,11 @@ parseEvent :: String -> Event
 parseEvent = match <*> splitOn '|'
       where
   match :: String -> [String] -> Event
-  match r (n:"P":from:to:[]) = Event r (read n) $ Message  (read from) (read to)
-  match r (n:"F":from:to:[]) = Event r (read n) $ Follow   (read from) (read to)
-  match r (n:"U":from:to:[]) = Event r (read n) $ Unfollow (read from) (read to)
-  match r (n:"S":from   :[]) = Event r (read n) $ Update   (read from)
-  match r (n:"B"        :[]) = Event r (read n) $ Broadcast
+  match r [n, "P", from, to] = Event r (read n) $ Message  (read from) (read to)
+  match r [n, "F", from, to] = Event r (read n) $ Follow   (read from) (read to)
+  match r [n, "U", from, to] = Event r (read n) $ Unfollow (read from) (read to)
+  match r [n, "S", from    ] = Event r (read n) $ Update   (read from)
+  match r [n, "B"          ] = Event r (read n)   Broadcast
   match _ _                  = error "Unrecognized event"
 
 splitOn :: Eq a => a -> [a] -> [[a]]
